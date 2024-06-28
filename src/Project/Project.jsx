fa1ae2eab0cbd138931a77780133c7f0246f3a0e
nambin <mkkim044@naver.com>
2024-06-28 22:55:02 +0900

    프로젝트 부분 완료

--- src/Project/Project.jsx
+++ src/Project/Project.jsx
@@ -1,60 +1,87 @@
-import { useEffect, useState } from 'react'
-import './Project.css'
+import { useEffect, useState } from 'react';
+import './Project.css';
 
-export default function Project(){
+export default function Project() {
 
-    //데이터 받아오기
-    const [data , setData] = useState([]);
-    const [url , setUrl] = useState('/data.json');
+  // 데이터 받아오기
+  const [data, setData] = useState([]); //이게 전체 데이터
+  const [url] = useState('/data.json'); //이게 데이터를 가져오는 출처
 
-    useEffect(()=> {
-        fetch(url)
-        .then(res=> {
-            if(!res.ok){
-                throw new Error("오류");
-            }
-            return res.json();
-        })
-        .then(json => setData(json))
-        .catch(err => console.error('에러발생', err));
-    },[url]);
+  const [selectedTitle, setSelectedTitle] = useState('밴러지'); //이게 선택된 데이터
 
-    return(
-        <div className="project" id="ProjectPage">
+    //데이터를 가져오는 로직
+  useEffect(() => {
+    fetch(url)
+      .then(res => {
+        if (!res.ok) {
+          throw new Error('오류');
+        }
+        return res.json();
+      })
+      .then(json => {
+        setData(json);
+      })
+  }, [url]);
+
+  //제목 선택
+  const handleTitleClick = (title) => {
+    setSelectedTitle(title);
+  };
+
+  //제목 클릭 시 해당 제목 선택
+  const projectTitles = data.map(project => project.title);
+  //선택된 제목의 내용에 맞는 거 찾기!!
+  const showData = data.find(project => project.title === selectedTitle);
+
+  return (
+    <div className="project" id="ProjectPage">
+      <div className='pj_top'>
         
-        <div className='pj_top'>
-            <h3>밴러지</h3>
-            <h3>이상형 월드컵</h3>
-            <h3>투 두 리스트</h3>
-            <h3>노드스타그램</h3>
-            <h3>TMI</h3>
-        </div>
+        {/* 이게 위의 제목들 */}
+        {projectTitles.map((title, index) => (
+                                                                        // 제목이 선택되면 selected 발동 아니면 ''
+          <h4 key={index} onClick={() => handleTitleClick(title)} className={selectedTitle === title ? 'selected' : ''}>
+            {title}
+          </h4>
+        ))}
+      </div>
 
-        <div className='pj_mid'>
+      <div className='pj_mid'>
         <div className='pj_mid_left'>
-          {data.map((v, index) => (
-            <div key={index}>
-              <h4>{v.title}</h4>
-              <img src={v.image} alt={v.title} />
-              <p>{v.review}</p>
+
+            {/* 데이터가 선택이 됐으면 ?  내용 : 프젝 선택하라 */}
+          {showData ? (
+            <div>
+              <h2 class='showData_title' >{showData.title}</h2>
+              <img class='showData_img' src={showData.image} alt='' />
+              <p class="showData_p0">{showData.review}</p>
             </div>
-          ))}
+          ) : (
+            <p>프로젝트를 선택하세요.</p>
+          )}
         </div>
 
+    
+    
+
+          {/* 데이터가 선택이 됐으면 ?  내용 : 프젝 선택하라 */}
+ {showData ? (
             <div className='pj_mid_right'>
-            소개 및 기능
-            개발기간
-            플랫폼
-           개발인원
-            나의역할
-            사용기술
-            깃허브
-            </div>
+            <div className='right'><p className='showData_p'>📍소개&기능</p><p class="showData_p2">{showData.intro}</p></div>
+            <div className='right'>  <p className='showData_p'>📍개발기간</p> <p class="showData_p2">{showData.period}</p></div>
+            <div className='right'>  <p className='showData_p'>📍플랫폼</p> <p class="showData_p2">{showData.platform}</p></div>
+            <div className='right'>  <p className='showData_p'>📍개발인원</p> <p class="showData_p2">{showData.personnel}</p></div>
+            <div className='right'>  <p className='showData_p'>📍사용 기술</p> <p class="showData_p3">{showData.skill}</p></div>
+            <div className='right'>  <p className='showData_p'>📍깃허브</p><a href={showData.git}><img className="SNS_img5" alt="깃허브" src="https://cdn-icons-png.flaticon.com/512/25/25231.png"></img></a></div>
+              </div>
+            ) : (
+              <p>프로젝트를 선택하세요.</p>
+            )}
+    
 
+
+
+</div>
         </div>
-
-
-
-        </div>
-    )
+  );
 }
