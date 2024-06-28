2f34cce2a134e64b3122850af04ce9004d0d2463
nambin <mkkim044@naver.com>
2024-06-28 17:35:05 +0900

    부분 수정

--- src/Project/Project.jsx
+++ src/Project/Project.jsx
@@ -1,6 +1,24 @@
+import { useEffect, useState } from 'react'
 import './Project.css'
 
 export default function Project(){
+
+    //데이터 받아오기
+    const [data , setData] = useState([]);
+    const [url , setUrl] = useState('/data.json');
+
+    useEffect(()=> {
+        fetch(url)
+        .then(res=> {
+            if(!res.ok){
+                throw new Error("오류");
+            }
+            return res.json();
+        })
+        .then(json => setData(json))
+        .catch(err => console.error('에러발생', err));
+    },[url]);
+
     return(
         <div className="project" id="ProjectPage">
         
@@ -13,11 +31,15 @@ export default function Project(){
         </div>
 
         <div className='pj_mid'>
-            <div className='pj_mid_left'>
-                    <h5>제목</h5>
-                    이미지
-                    후기
+        <div className='pj_mid_left'>
+          {data.map((v, index) => (
+            <div key={index}>
+              <h4>{v.title}</h4>
+              <img src={v.image} alt={v.title} />
+              <p>{v.review}</p>
             </div>
+          ))}
+        </div>
 
             <div className='pj_mid_right'>
             소개 및 기능
